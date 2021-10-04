2bd03b41d7bd7b97f73d13c257d91e0416e3ed38
Bang Nguyen <luongbang152@gmail.com>
2021-10-04 16:14:01 +0700

    fix theme to use swatch

--- lib/core/theme/light_theme_data.dart
+++ lib/core/theme/light_theme_data.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 
+final MaterialColor _primarySwatch = Colors.yellow;
 final Color _primaryColor = Colors.yellow[700]!;
 final Color _appBarColor = Colors.grey[100]!;
 final Color _appBarIconColor = Colors.black87;
 final Color _foreGroundColor = Colors.white;
 
 var lightThemeData = ThemeData(
-  cursorColor: _primaryColor,
-  textSelectionColor: _primaryColor,
   brightness: Brightness.light,
   scaffoldBackgroundColor: Colors.grey[200],
   primaryColor: _primaryColor,
+  primarySwatch: _primarySwatch,
   indicatorColor: _primaryColor,
-  accentColor: _primaryColor,
-  buttonColor: _primaryColor,
   iconTheme: IconThemeData(color: _primaryColor),
   visualDensity: VisualDensity.adaptivePlatformDensity,
   appBarTheme: AppBarTheme(
